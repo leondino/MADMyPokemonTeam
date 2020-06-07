a0cb3bd01903ce59c69ba0828d259167903a8c44
Léon Imanuel <leondino0309@gmail.com>
2020-06-07 20:23:24 +0200

    Implemented the splash screen

--- app/src/main/java/com/example/mypokemonteam/ui/SplashActivity.kt
+++ app/src/main/java/com/example/mypokemonteam/ui/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mypokemonteam.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.mypokemonteam.R
 
 class SplashActivity : AppCompatActivity() {
@@ -9,5 +11,14 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(
+                Intent(
+                    this@SplashActivity,
+                    MainActivity::class.java
+                )
+            )
+            finish()
+        }, 1000)
     }
 }
