cd70213e849f286275cfdc2546dc22f43f6c88b7
Léon Imanuel <leondino0309@gmail.com>
2020-06-07 20:05:59 +0200

    Added fonts and drawable for splash screen Added splash screen layout

---
+++ app/src/main/java/com/example/mypokemonteam/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mypokemonteam.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mypokemonteam.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
