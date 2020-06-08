1d108f1d5dc9cd8c47d133af7298ab5ab39b2485
Léon Imanuel <leondino0309@gmail.com>
2020-06-08 11:55:10 +0200

    Added second menu in toolbar rop right corner with 'delete all' option. (Not implemented yet)

--- app/src/main/java/com/example/mypokemonteam/ui/PokemonActivity.kt
+++ app/src/main/java/com/example/mypokemonteam/ui/PokemonActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mypokemonteam.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -40,5 +42,24 @@ class PokemonActivity : AppCompatActivity() {
         toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_delete, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.deleteAll-> {
+                //deleteGames()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
 }
