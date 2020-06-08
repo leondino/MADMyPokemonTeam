339a6a71a78116384421ae64c85fda7ac74bc78e
Léon Imanuel <leondino0309@gmail.com>
2020-06-08 11:30:31 +0200

    Added navigation through with bottem navigation menu

--- app/src/main/java/com/example/mypokemonteam/ui/PokemonActivity.kt
+++ app/src/main/java/com/example/mypokemonteam/ui/PokemonActivity.kt
@@ -3,9 +3,14 @@ package com.example.mypokemonteam.ui
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.mypokemonteam.R
 
 import kotlinx.android.synthetic.main.activity_pokemon.*
+import kotlinx.android.synthetic.main.content_pokemon.*
 
 class PokemonActivity : AppCompatActivity() {
 
@@ -13,7 +18,27 @@ class PokemonActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon)
         setSupportActionBar(toolbar)
+        initViews()
+        initViewModel()
+        initNavigation()
+    }
+
+    private fun initViews(){
 
     }
+
+    private fun initViewModel(){
+
+    }
+
+    private fun initNavigation() {
+        val navController = findNavController(R.id.navHostFragment)
+
+        NavigationUI.setupWithNavController(navView, navController)
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
 
 }
